7f387e5682c04c36cb37d1b8d661a590721b2438
asqatester <52762318+asqatester@users.noreply.github.com>
2024-07-28 17:07:19 -0300

    Rresolvendo desafio Lista tarefa Livro e Catalogo
    Signed-off-by: asqatester <52762318+asqatester@users.noreply.github.com>

--- 19-collections-java/CollectionsJava/src/main/java/list/Pesquisa/CatalogoLivros.java
+++ 19-collections-java/CollectionsJava/src/main/java/list/Pesquisa/CatalogoLivros.java
@@ -65,25 +65,32 @@ public class CatalogoLivros {
     // Adicionando livros ao catálogo
     catalogoLivros.adicionarLivro("Microsserviços Prontos Para a Produção", "Susan J. Fowler", 2017);
     catalogoLivros.adicionarLivro("Java Guia do Programador", "Peter Jandl Junior", 2021);
-    catalogoLivros.adicionarLivro("Código Limpo", "Robert C. Martin", 2009);
+    catalogoLivros.adicionarLivro("Código Limpo", "Robert C. Martin", 2010);
+    catalogoLivros.adicionarLivro("O Programador Pragmático", "Andy Hunt e Dave Thomas", 2009);
     catalogoLivros.adicionarLivro("O Codificador Limpo", "Robert C. Martin", 2012);
 
     // Exibindo livros pelo mesmo autor
+    System.out.println("******** PESQUISA POR MESMO AUTOR:");
     System.out.println(catalogoLivros.pesquisarPorAutor("Robert C. Martin"));
 
     // Exibindo livros pelo mesmo autor (caso em que não há livros de um autor específico)
+    System.out.println("******** PESQUISA POR UM AUTOR INXISTENTE (retorna vazio):");
     System.out.println(catalogoLivros.pesquisarPorAutor("Autor Inexistente"));
 
     // Exibindo livros dentro de um intervalo de anos
+    System.out.println("******** PESQUISA POR INTERVALO DE ANOS ENTRE 2010 E 2022: ");
     System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2010, 2022));
 
     // Exibindo livros dentro de um intervalo de anos (caso em que não há livros no intervalo)
+    System.out.println("******** PESQUISA POR INTERVALO DE ANOS ENTRE 2025 E 2030 (retorna vazio): ");
     System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2025, 2030));
 
     // Exibindo livros por título
+    System.out.println("******** PESQUISA POR TÍTULO:");
     System.out.println(catalogoLivros.pesquisarPorTitulo("Java Guia do Programador"));
 
     // Exibindo livros por título (caso em que não há livros com o título especificado)
+    System.out.println("******** PESQUISA POR UM TÍTULO INEXISTENTE (retorna null pois é o valor inicial do objeto Livro :");
     System.out.println(catalogoLivros.pesquisarPorTitulo("Título Inexistente"));
   }
 }
