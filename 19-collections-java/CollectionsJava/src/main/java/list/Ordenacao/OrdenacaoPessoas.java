58c93cb247d37dcaee2d3b96b5d5f4d5b81ab148
asqatester <52762318+asqatester@users.noreply.github.com>
2024-07-28 19:42:21 -0300

    Melhorando a exibição da saída no log com quebra de linha
    Signed-off-by: asqatester <52762318+asqatester@users.noreply.github.com>

--- 19-collections-java/CollectionsJava/src/main/java/list/Ordenacao/OrdenacaoPessoas.java
+++ 19-collections-java/CollectionsJava/src/main/java/list/Ordenacao/OrdenacaoPessoas.java
@@ -42,7 +42,17 @@ public class OrdenacaoPessoas {
 
     // Adicionando pessoas à lista
     ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
+    ordenacaoPessoas.adicionarPessoa("Yara", 30, 1.71);
+    ordenacaoPessoas.adicionarPessoa("Zane", 22, 1.84);
+    ordenacaoPessoas.adicionarPessoa("Alex", 27, 1.69);
+    ordenacaoPessoas.adicionarPessoa("Brian", 35, 1.87);
+    ordenacaoPessoas.adicionarPessoa("Cindy", 24, 1.57);
     ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
+    ordenacaoPessoas.adicionarPessoa("John", 40, 1.85);
+    ordenacaoPessoas.adicionarPessoa("Kate", 27, 1.60);
+    ordenacaoPessoas.adicionarPessoa("Liam", 21, 1.75);
+    ordenacaoPessoas.adicionarPessoa("Maya", 32, 1.68);
+    ordenacaoPessoas.adicionarPessoa("Noah", 18, 1.80);
     ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
     ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);
 
@@ -50,9 +60,45 @@ public class OrdenacaoPessoas {
     System.out.println(ordenacaoPessoas.pessoaList);
 
     // Ordenando e exibindo por idade
-    System.out.println(ordenacaoPessoas.ordenarPorIdade());
+    //System.out.println(ordenacaoPessoas.ordenarPorIdade());
 
+    // ORDENANDO E EXIBINDO POR IDADE
+    try {
+        List<Pessoa> pessoasOrdenadas = ordenacaoPessoas.ordenarPorIdade();
+        System.out.println("************** ORDENAÇÃO DAS PESSOAS POR IDADE *******");
+        // Imprime cada pessoa em uma nova linha
+        for (Pessoa pessoa : pessoasOrdenadas) {
+            System.out.println(pessoa);
+        }
+    } catch (RuntimeException e) {
+        System.out.println(e.getMessage());
+    }
+    
+    
+    
     // Ordenando e exibindo por altura
-    System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    //System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    
+    // Supondo que você já tem uma lista de pessoas e um método de ordenação
+    //ordenacaoPessoas = new OrdenacaoPessoas();
+    
+    // ORDENANDO E EXIBINDO POR ALTURA
+    try {
+        List<Pessoa> pessoasOrdenadas = ordenacaoPessoas.ordenarPorAltura();
+        System.out.println("************** ORDENAÇÃO DAS PESSOAS POR ALTURA *******");
+        // Imprime cada pessoa em uma nova linha
+        for (Pessoa pessoa : pessoasOrdenadas) {
+            System.out.println(pessoa);
+        }
+    } catch (RuntimeException e) {
+        System.out.println(e.getMessage());
+    }
+    
+    
+    
+    
+    
+    
+    
   }
 }
